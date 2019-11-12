5db44892335314525bf5d65962a9a64fbd3b2087
zhongjn <2389206378@qq.com>
2019-11-12 09:11:46 +0800

    temp

--- PCPU/Control.v
+++ PCPU/Control.v
@@ -18,26 +18,28 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
-module Control(input wire [5:0]op,
-					input wire [4:0]rs,
-					input wire [4:0]rt,
-					input wire [5:0]func,
+module Control(input wire [5:0] op,
+					input wire [4:0] rs,
+					input wire [4:0] rt,
+					input wire [5:0] func,
+					input wire rsrtequ,
 					output wire jal,
 					output wire wreg,
+					output wire branch,
 					output wire mem2reg,
 					output wire wmem,
-					output wire alu[3:0],
+					output wire [3:0] aluc,
 					output wire aluimm,
 					output wire shift,
-					output wire rsrtequ,
 					output wire sext,
+					output wire jr, 
 					output wire regrt,
-					output wire [1:0]fwdb,
-					output wire [1:0]fwda,
+					output wire [1:0] fwdb,
+					output wire [1:0] fwda,
 					output wire stall,
-					output wire flush
-					
-    );
+					output wire jump,
+					output wire remain_pc
+);
 	 parameter INS_R = 6'b000000, RFUNC_SLL = 6'b000000, RFUNC_SRL = 6'b000010, RFUNC_JR = 6'b001000, RFUNC_ADD = 6'b100000, RFUNC_SUB = 6'b100010, RFUNC_AND = 6'b100100,
 	 		   RFUNC_OR = 6'b100101, RFUNC_XOR = 6'b100110, RFUNC_NOR = 6'b100111, RFUNC_SLT = 6'b101010;
 	 always@(*)
--- PCPU/PCPU.v
+++ PCPU/PCPU.v
@@ -1,37 +1,79 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    20:27:20 11/11/2019 
-// Design Name: 
-// Module Name:    PCPU 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
 module PCPU(
-	input wire INT,
-	input wire clk,
-	input wire reset,
-	input wire MIO_ready,
-	input wire [31:0] Data_in,
-	output wire mem_w,
-	output wire [31:0] PC_out,
-	output wire [31:0] inst_out,
-	output wire [31:0] Data_out,
-	output wire [31:0] Addr_out,
-	output wire CPU_MIO,
-	output wire [4:0] state
-    );
-// TODO
+    input wire INT,
+    input wire clk,
+    input wire reset,
+    input wire MIO_ready,
+    input wire [31:0] data_mem,
+    input wire [31:0] inst_mem,
+    output wire mem_w,
+    output wire [31:0] inst_addr
+    output wire [31:0] data_addr,
+    output wire CPU_MIO,
+);
+
+    // control wires
+    wire jal, wreg, branch, m2reg, wmem, aluimm, shift, sext, jr, reg_rt, stall, jump, remain_pc;
+    wire [3:0] aluc;
+    wire [1:0] fwda, fwdb;
+
+    // PC
+    reg [31:0] pc;
+
+    // IF wires
+    wire [31:0] pc_new;
+
+    // IF/ID pipeline register
+    reg [31:0] ifid_ir, ifid_pc;
+
+    // ID wires
+    wire [31:0] branch_target;
+
+    // ID/EX pipeline register
+    reg [31:0] idex_ir;
+    reg [3:0] idex_aluc;
+    reg idex_wreg, idex_mreg, idex_wmem, idex_aluimm, idex_shift;
+    reg [31:0] idex_rs_data, idex_rt_data;
+    reg [31:0] idex_imm_ext;
+    reg [4:0] idex_wb_reg;
+
+    // EX/MEM pipeline register
+    reg [31:0] exmem_ir;
+    reg exmem_wreg, exmem_m2reg, exmem_wmem;
+    reg [31:0] exmem_alu_result, idex_rt_data;
+    reg [4:0] exmem_wb_reg;
+
+    // MEM/WB pipeline register
+    reg [31:0] memwb_ir;
+    reg memwb_wreg, memwb_m2reg;
+    reg [31:0] memwb_data, memwb_alu_result;
+    reg [4:0] memwb_wb_reg;
+
+    // output
+    assign inst_addr = pc;
+
+    // IF wires
+    assign pc_new = branch ? branch_target : pc + 4'h4;
+
+    // ID wires
+    assign branch_target = 0; // TODO
+    wire [5:0] func = ifid_ir[5:0];
+    wire [5:0] op = ifid_ir[31:26];
+    wire [4:0] rs = ifid_ir[25:21];
+    wire [4:0] rt = ifid_ir[20:16];
+    wire [15:0] imm = ifid_ir[15:0];
+    wire [31:0] j_addr = { ifid_pc[31:28], ifid_ir[25:0], 2'h0 };
+    wire [31:0] br_addr = ifid_pc + {imm, 2'h0};
+    wire [31:0] jr_addr = 0; // TODO
+    
+    always @(posedge clk) begin
+
+        pc <= remain_pc ? pc : pc_new;
+
+        // IF/ID
+        ifid_ir <= stall ? 0 : inst_mem;
+        ifid_pc <= pc + 4'h4;
+    end
+
 
 endmodule
