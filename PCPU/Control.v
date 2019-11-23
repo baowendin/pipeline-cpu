2606f39d7f77f2554f014c068d2b2978936cdc24
baowendin <46412693+baowendin@users.noreply.github.com>
2019-11-23 20:34:20 +0800

    Update Control.v

--- PCPU/Control.v
+++ PCPU/Control.v
@@ -1,4 +1,3 @@
-`timescale 1ns / 1ps
 module Control(
 	input wire [5:0] op,
 	input wire [4:0] rs,
@@ -30,6 +29,7 @@ module Control(
 	output reg cancel_next
 );
 	reg rs_used, rt_used;
+    reg branch_t, wreg_t, wmem_t;
 	parameter INS_R = 6'b000000, RFUNC_SLL = 6'b000000, RFUNC_SRL = 6'b000010, RFUNC_JR = 6'b001000, RFUNC_ADD = 6'b100000, RFUNC_SUB = 6'b100010, RFUNC_AND = 6'b100100,
 			RFUNC_OR = 6'b100101, RFUNC_XOR = 6'b100110, RFUNC_NOR = 6'b100111, RFUNC_SLT = 6'b101010;
 	parameter  INS_J = 6'b000010, INS_JAL = 6'b000011, INS_BEQ = 6'b000100, INS_BNE = 6'b000101, INS_LUI = 6'b001111;
@@ -39,13 +39,13 @@ module Control(
 	always @(*) begin
 		rs_used = 0;
 		rt_used = 0;
-		branch = 0;
+		branch_t = 0;
 		jr = 0;
 		jump = 0;
 		jal = 0;
-		wreg = 0;
+		wreg_t = 0;
 		mem2reg = 0;
-		wmem = 0;
+		wmem_t = 0;
 		aluc = 0;
 		aluimm = 0;
 		shift = 0;
@@ -58,53 +58,53 @@ module Control(
 						aluc = ALUC_SRL;
 						shift = 1'b1;
 						regrt = 1'b1;
-						wreg = 1'b1;
+						wreg_t = 1'b1;
 						rt_used = 1;
 					end
 					RFUNC_JR: begin
 						// mem2reg = 1'b1;
 						rs_used = 1'b1;
-						branch = 1'b1;
+						branch_t = 1'b1;
 						jr = 1;
 					end
 					RFUNC_ADD: begin
 						aluc = ALUC_ADD;
 						regrt = 1'b1;
-						wreg = 1'b1;
+						wreg_t = 1'b1;
 						rs_used = 1;
 						rt_used = 1;
 					end
 					RFUNC_SUB: begin
 						regrt = 1'b1;
-						wreg = 1'b1;
+						wreg_t = 1'b1;
 						aluc = ALUC_SUB;	
 						rs_used = 1'b1;
 						rt_used = 1'b1;					
 					end
 					RFUNC_AND: begin
 						aluc = ALUC_AND;
-						wreg = 1'b1;
+						wreg_t = 1'b1;
 						regrt = 1'b1;
 						rs_used = 1'b1;
 						rt_used = 1'b1;
 					end
 					RFUNC_OR: begin
 						aluc = ALUC_OR;
-						wreg = 1'b1;
+						wreg_t = 1'b1;
 						regrt = 1'b1;
 						rs_used = 1'b1;
 						rt_used = 1'b1;
 					end
 					RFUNC_XOR: begin
 						aluc = ALUC_XOR;
-						wreg = 1'b1;
+						wreg_t = 1'b1;
 						regrt = 1'b1;
 						rs_used = 1'b1;
 						rt_used = 1'b1;
 					end
 					RFUNC_SLT: begin
 						aluc = ALUC_SLT;
-						wreg = 1;
+						wreg_t = 1;
 						regrt = 1'b1;
 						rs_used = 1'b1;
 						rt_used = 1'b1;
@@ -113,19 +113,19 @@ module Control(
 			end
 			INS_J: begin
 				jump = 1'b1;
-				branch = 1'b1;
+				branch_t = 1'b1;
 			end
 			INS_JAL: begin
 				jal = 1'b1;
 				jump = 1;
-				wreg = 1'b1;
+				wreg_t = 1'b1;
 				aluc = ALUC_ADD;
-				branch = 1'b1;
+				branch_t = 1'b1;
 			end
 			INS_BEQ: begin
 				aluc = ALUC_ADD;
 				aluimm = 1'b1;
-				if (rsrtequ == 1'b1) branch = 1'b1;
+				if (rsrtequ == 1'b1) branch_t = 1'b1;
 				sext = 1'b1;
 				rs_used = 1'b1;
 				rt_used = 1'b1;
@@ -133,7 +133,7 @@ module Control(
 			INS_BNE: begin
 				aluc = ALUC_ADD;
 				aluimm = 1'b1;
-				if (rsrtequ == 1'b0) branch = 1'b1;
+				if (rsrtequ == 1'b0) branch_t = 1'b1;
 				sext = 1'b1;
 				rs_used = 1'b1;
 				rt_used = 1'b1;
@@ -142,31 +142,31 @@ module Control(
 				aluc = ALUC_ADD;
 				aluimm = 1'b1;
 				sext = 1'b1;
-				wreg = 1'b1;
+				wreg_t = 1'b1;
 				rs_used = 1'b1;				
 			end
 			INS_ANDI: begin
 				aluc = ALUC_AND;
 				aluimm = 1'b1;
-				wreg = 1'b1;
+				wreg_t = 1'b1;
 				rs_used = 1'b1;			
 			end
 			INS_ORI: begin
 				aluc = ALUC_OR;
 				aluimm = 1'b1;
-				wreg = 1'b1;	
+				wreg_t = 1'b1;	
 				rs_used = 1'b1;			
 			end
 			INS_XORI: begin
 				aluc = ALUC_XOR;
 				aluimm = 1'b1;
-				wreg = 1'b1;	
+				wreg_t = 1'b1;	
 				rs_used = 1'b1;			
 			end
 			INS_LUI: begin
 				aluc = ALUC_LUI;			
 				aluimm = 1;	
-				wreg = 1'b1;
+				wreg_t = 1'b1;
 			end
 			INS_LW: begin
 				aluc = ALUC_ADD;
@@ -174,25 +174,27 @@ module Control(
 				sext = 1'b1;
 				mem2reg = 1'b1;
 				rs_used = 1'b1;
-				wreg = 1'b1;
+				wreg_t = 1'b1;
 			end
 			INS_SW: begin
 				aluc = ALUC_ADD;
 				aluimm = 1'b1;
 				sext = 1'b1;
-				wmem = 1'b1;
+				wmem_t = 1'b1;
 				rs_used = 1'b1;
 				rt_used = 1'b1;
 			end
-		endcase
-		branch = stall ? 0 : branch;
-		wreg = stall ? 0 : wreg;
-		wmem = stall ? 0 : wmem;
+		endcase	
+       
+
 	end
 
 	// the stall situation, including two hazards
 	always @(*) begin
 		stall = 0;
+		branch = 0;
+		wreg = 0;
+		wmem = 0;
 		cancel_next = 0;
 		if (rs_used && rs != 0) begin
 			if (exe_regw_addr == rs && exe_mem2reg) begin
@@ -207,8 +209,10 @@ module Control(
 		else if (branch) begin
 			cancel_next = 1;
 		end
+		branch = stall ? 0 : branch_t;
+       		wreg = stall ? 0 : wreg_t;
+       		wmem = stall ? 0 : wmem_t;
 	end
-
 
 	// the forward situation
 	always @(*) begin
